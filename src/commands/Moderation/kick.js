27d0859e5d48db045efe4f1d9c25e18e3b45b208
theoreotm <qwertzybad@gmail.com>
2024-03-02 08:45:14 +0500

    feat: use emojis and colors from config

--- src/commands/Moderation/kick.js
+++ src/commands/Moderation/kick.js
@@ -1,3 +1,4 @@
+const { emojis, color } = require('../../config');
 const BeemoCommand = require('../../lib/structures/commands/BeemoCommand');
 
 class UserCommand extends BeemoCommand {
@@ -41,22 +42,22 @@ class UserCommand extends BeemoCommand {
 		// }
 		if (!kickMember) {
 			return await interaction.reply({
-				content: '<:bl_x_mark:1206436599794241576> The user **mentioned** is no longer **within** the **server**!',
+				content: `${emojis.custom.fail} The user **mentioned** is no longer **within** the **server**!`,
 				ephemeral: true
 			});
 		}
 		if (!kickMember.kickable) {
 			return await interaction.reply({
-				content: '<:bl_x_mark:1206436599794241576> I **cannot** kick this user because they are either **higher** than me or you!',
+				content: `${emojis.custom.fail} I **cannot** kick this user because they are either **higher** than me or you!`,
 				ephemeral: true
 			});
 		}
 		if (interaction.member.id === kickMember.id) {
-			return interaction.reply({ content: '<:bl_x_mark:1206436599794241576> You cannot kick yourself!', ephemeral: true });
+			return interaction.reply({ content: `${emojis.custom.fail} You cannot kick yourself!`, ephemeral: true });
 		}
 		if (kickMember.permissions.has(PermissionsBitField.Flags.Administrator)) {
 			return interaction.reply({
-				content: '<:bl_x_mark:1206436599794241576> You **cannot** kick **staff members** or people with the **Administrator** permission!',
+				content: `${emojis.custom.fail} You **cannot** kick **staff members** or people with the **Administrator** permission!`,
 				ephemeral: true
 			});
 		}
@@ -64,7 +65,7 @@ class UserCommand extends BeemoCommand {
 		// DM Message
 		try {
 			const dmEmbed = {
-				color: '#ff5555',
+				color: `#ff5555`,
 				title: `🚫 You have been kicked from **${interaction.guild.name}**`,
 				thumbnail: { url: interaction.guild.iconURL() },
 				fields: [
@@ -78,7 +79,7 @@ class UserCommand extends BeemoCommand {
 
 			// Kick Successful
 			const kickConfirmationEmbed = {
-				color: '#00ff00',
+				color: `${color.success}`,
 				title: '`✅` Kick Successful',
 				description: `**${userToKick.tag}** has been **kicked** from the server.`,
 				fields: [{ name: '**Reason:**', value: reason }],
@@ -92,7 +93,7 @@ class UserCommand extends BeemoCommand {
 		} catch (error) {
 			console.error(error);
 			const errorEmbed = {
-				color: '#ff0000',
+				color: `${color.fail}`,
 				title: '`❌` Error Kicking User',
 				description: `I have **Failed** to kick **${userToKick.tag}** from the server.`,
 				timestamp: new Date(),
