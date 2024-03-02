c50751729f2fffd6ff1c62e8a5f62924feb563ed
theoreotm <qwertzybad@gmail.com>
2024-03-02 08:52:57 +0500

    fix: change name from `lock` to `unlock`

--- src/commands/Moderation/unlock.js
+++ src/commands/Moderation/unlock.js
@@ -1,7 +1,7 @@
 const BeemoCommand = require('../../lib/structures/commands/BeemoCommand');
 const { PermissionLevels } = require('../../lib/types/Enums');
-const { color, emojis } = require('../../config')
-const { EmbedBuilder, PermissionsBitField, ChannelType  } = require('discord.js');
+const { color, emojis } = require('../../config');
+const { EmbedBuilder, PermissionsBitField, ChannelType } = require('discord.js');
 
 class UserCommand extends BeemoCommand {
 	/**
@@ -11,7 +11,7 @@ class UserCommand extends BeemoCommand {
 	constructor(context, options) {
 		super(context, {
 			...options,
-      requiredUserPermissions: ['ManageChannels'],
+			requiredUserPermissions: ['ManageChannels'],
 			description: 'Unlock a channel for a specific role'
 		});
 	}
@@ -22,18 +22,15 @@ class UserCommand extends BeemoCommand {
 	registerApplicationCommands(registry) {
 		registry.registerChatInputCommand((builder) =>
 			builder //
-				.setName('lock')
+				.setName('unlock')
 				.setDescription(this.description)
-                .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageChannels)
-                .addChannelOption(option => 
-                    option.setName('channel')
-                    .setDescription('The channel you want to lock')
-                    .addChannelTypes(ChannelType.GuildText)
-                    .setRequired(true))
-                .addRoleOption(option => 
-                    option.setName('role')
-                    .setDescription('The role you want to restrict from sending messages')
-                    .setRequired(true)),
+				.setDefaultMemberPermissions(PermissionsBitField.Flags.ManageChannels)
+				.addChannelOption((option) =>
+					option.setName('channel').setDescription('The channel you want to lock').addChannelTypes(ChannelType.GuildText).setRequired(true)
+				)
+				.addRoleOption((option) =>
+					option.setName('role').setDescription('The role you want to restrict from sending messages').setRequired(true)
+				)
 		);
 	}
 
@@ -41,40 +38,52 @@ class UserCommand extends BeemoCommand {
 	 * @param {BeemoCommand.ChatInputCommandInteraction} interaction
 	 */
 	async chatInputRun(interaction) {
-    // Retrieving channel and role from interaction options
-    const channel = interaction.options.getChannel('channel');
-    const role = interaction.options.getRole('role');
+		// Retrieving channel and role from interaction options
+		const channel = interaction.options.getChannel('channel');
+		const role = interaction.options.getRole('role');
 
-    // Permissions
+		// Permissions
 		// if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels))
 		// 	return await interaction.reply({
 		// 		content: `${emojis.custom.fail} You are not **authorized** to **execute** this command!`,
 		// 		ephemeral: true
 		// 	});
 
-    // Setting permission overwrites to allow sending messages
-    channel.permissionOverwrites.create(role.id, { SendMessages: true });
+		// Setting permission overwrites to allow sending messages
+		channel.permissionOverwrites.create(role.id, { SendMessages: true });
 
-    // Success Embed construction
-    const embed = new EmbedBuilder()
-      .setTitle('`🔒` Channel Unlocked')
-      .setDescription(`${emojis.custom.success} ${channel} has been **unlocked**.`)
-      .addFields(
-        { name: '`🔒` Unlocked for Role:', value: `
-        <:bl_Reply:1212047469014425650> ${role}`, inline: false },
-        { name: '<:bl_clock:1206612806560915547> Time:', value: `
-        <:bl_Reply:1212047469014425650> <t:${new Date()}:f>`, inline: false },
-        { name: '`🔒` Unlocked by:', value: `
-        <:bl_Reply:1212047469014425650> <@${interaction.user.id}>`, inline: false },
-      )
-      .setColor(`${color.default}`)
-      .setTimestamp()
-      .setFooter({ text: `${interaction.user.displayName}`, iconURL: interaction.user.displayAvatarURL() });
+		// Success Embed construction
+		const embed = new EmbedBuilder()
+			.setTitle('`🔒` Channel Unlocked')
+			.setDescription(`${emojis.custom.success} ${channel} has been **unlocked**.`)
+			.addFields(
+				{
+					name: '`🔒` Unlocked for Role:',
+					value: `
+        <:bl_Reply:1212047469014425650> ${role}`,
+					inline: false
+				},
+				{
+					name: '<:bl_clock:1206612806560915547> Time:',
+					value: `
+        <:bl_Reply:1212047469014425650> <t:${new Date()}:f>`,
+					inline: false
+				},
+				{
+					name: '`🔒` Unlocked by:',
+					value: `
+        <:bl_Reply:1212047469014425650> <@${interaction.user.id}>`,
+					inline: false
+				}
+			)
+			.setColor(`${color.default}`)
+			.setTimestamp()
+			.setFooter({ text: `${interaction.user.displayName}`, iconURL: interaction.user.displayAvatarURL() });
 
-    // Replying with the success embed
-    await interaction.reply({ embeds: [embed] });
-  }
-};
+		// Replying with the success embed
+		await interaction.reply({ embeds: [embed] });
+	}
+}
 
 module.exports = {
 	UserCommand
