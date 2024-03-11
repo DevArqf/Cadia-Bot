92eaeb5b3d3baac4445340abcef51d9a73e68f15
theoreotm <qwertzybad@gmail.com>
2024-03-11 23:40:51 +0500

    feat: blacklist precondition

---
+++ src/preconditions/global/Blacklist.js
@@ -0,0 +1,44 @@
+const { Precondition } = require('@sapphire/framework');
+const { ChatInputCommandInteraction } = require('discord.js');
+const { envParseArray } = require('@skyra/env-utilities');
+const { GuildMessage } = require('../../lib/types/Discord');
+
+/**
+ * @class
+ * @extends {Precondition}
+ */
+class BotOwnerPrecondition extends Precondition {
+	/**
+	 * @param {GuildMessage} message
+	 * @returns {import('@sapphire/framework').PreconditionResult}
+	 */
+	messageRun(message) {
+		return this.isBlacklisted(message.guildId) ? this.ok() : this.error({ context: { silent: false }, identifier: 'PermissionError' });
+	}
+
+	/**
+	 * @param {ChatInputCommandInteraction<'cached'>} interaction
+	 * @returns {import('@sapphire/framework').PreconditionResult}
+	 */
+	chatInputRun(interaction) {
+		return this.isBlacklisted(interaction.guildId) ? this.ok() : this.error({ context: { silent: false }, identifier: 'PermissionError' });
+	}
+
+	/**
+	 * @param {import('../../lib/types/Discord').GuildContextMenuInteraction} interaction
+	 * @returns {import('@sapphire/framework').PreconditionResult}
+	 */
+	contextMenuRun(interaction) {
+		return this.isBlacklisted(interaction.guildId) ? this.ok() : this.error({ context: { silent: false }, identifier: 'PermissionError' });
+	}
+
+	/**
+	 *
+	 * @param {import('discord.js').Snowflake} guildId The id of the guild you want to check
+	 */
+	isBlacklisted(guildId) {
+		return false; // TODO: Implement this
+	}
+}
+
+module.exports = { BotOwnerPrecondition };
