5dafd2f16066105099977699783dd512b592d6a1
DevArqf <malik.magicly@gmail.com>
2024-03-16 16:19:46 -0400

    Bolded error message in ErrorHandler

--- src/listeners/commands/chatInputCommandError.js
+++ src/listeners/commands/chatInputCommandError.js
@@ -24,7 +24,7 @@ class UserEvent extends Listener {
 
 		await interaction.client.channels.cache.get(channels.errorLogging).send({ embeds: [errorEmbed] });
 
-		return interaction.reply(`${emojis.custom.fail} I have encountered an error! Please try again later.`);
+		return interaction.reply(`${emojis.custom.fail} **I have encountered an error! Please try again later.**`);
 
 	}
 }
