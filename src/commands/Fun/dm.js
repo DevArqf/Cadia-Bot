5a5843bc1636210f17a009f5af9a83cfccf7279b
DevArqf <malik.magicly@gmail.com>
2024-03-19 17:04:39 -0400

    Fixed bug where the main logic got deleted

--- src/commands/Fun/dm.js
+++ src/commands/Fun/dm.js
@@ -1,64 +1,136 @@
 const BeemoCommand = require('../../lib/structures/commands/BeemoCommand');
-const { PermissionLevels } = require('../../lib/types/Enums');
+const { PermissionFlagsBits } = require('discord.js');
 const { color, emojis } = require('../../config');
-const { EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+const { UserSettingsSchema } = require('../../lib/schemas/usersettings');
 
 class UserCommand extends BeemoCommand {
-	/**
-	 * @param {BeemoCommand.Context} context
-	 * @param {BeemoCommand.Options} options
-	 */
-	constructor(context, options) {
-		super(context, {
-			...options,
-			description: 'Send a Anonymous DM to a user within the server'
-		});
-	}
+    /**
+     * @param {BeemoCommand.Context} context
+     * @param {BeemoCommand.Options} options
+     */
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            description: "Send a Anonymous DM to a user within the server"
+        });
+    }
 
-	/**
-	 * @param {BeemoCommand.Registry} registry
-	 */
-	registerApplicationCommands(registry) {
-		registry.registerChatInputCommand((builder) =>
-			builder //
-				.setName('dm')
-				.setDescription(this.description)
-				.addUserOption((option) => option.setName('user').setDescription('The user that should receive a DM').setRequired(true))
-				.addStringOption((option) => option.setName('message').setDescription('The message that the user should receive').setRequired(true))
-		);
-	}
+    /**
+     * @param {BeemoCommand.Registry} registry
+     */
+    registerApplicationCommands(registry) {
+        registry.registerChatInputCommand((builder) =>
+            builder //
+                .setName('dm')
+                .setDescription(this.description)
+                .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+                .addSubcommand((subcommand) =>
+                    subcommand //
+                        .setName('send')
+                        .setDescription('Send a direct message to a user')
+                        .addUserOption(option =>
+                            option
+                                .setName('user')
+                                .setDescription('The user that should receive a DM')
+                                .setRequired(true))
+                        .addStringOption(option =>
+                            option
+                                .setName('message')
+                                .setDescription('The message that the user should receive')
+                                .setRequired(true)))
+                .addSubcommand((subcommand) =>
+                    subcommand //
+                        .setName('toggle')
+                        .setDescription('Toggle to receive DMs or not')
+                        .addStringOption(option =>
+                            option
+                                .setName('action')
+                                .setDescription('Choose to opt-in or opt-out of receiving DMs')
+                                .setRequired(true)
+                                .addChoices(
+                                    { name: "opt-out", value: "opt-out" },
+                                    { name: "opt-in", value: "opt-in" }
+                                )
+                        )
+                )
+        );
+    }
 
-	/**
-	 * @param {BeemoCommand.ChatInputCommandInteraction} interaction
-	 */
-	async chatInputRun(interaction) {
-		const user = interaction.options.getUser('user');
-		const message = interaction.options.getString('message');
+    /**
+     * @param {BeemoCommand.ChatInputCommandInteraction} interaction
+     */
+    async chatInputRun(interaction) {
+        const subcommand = interaction.options.getSubcommand();
+    
+        if (subcommand === 'send') {
+            const user = interaction.options.getUser('user');
+            const message = interaction.options.getString('message');
+            
+            // Check if the user has opted out
+            const userId = user.id;
+            const userSettings = await UserSettingsSchema.findOne({ userId });
+            if (userSettings && !userSettings.receiveDMs) {
+                interaction.reply({ content: `${emojis.custom.fail} **${user.tag}** has **opted out** and can **no longer** receive DMs.`, ephemeral: true });
+                return;
+            }
+    
+            const embed = new EmbedBuilder()
+                .setTitle('`📮` DM Received')
+                .setColor(`${color.default}`)
+                .addFields(
+                    { name: '`📝` Message', value: `${emojis.custom.replyend} ${message}` },
+                    { name: '`💻` Server', value: `${emojis.custom.replyend} ${interaction.guild.name}` },
+                    { name: '`👤` Author', value: `${emojis.custom.replyend} <@${interaction.user.id}>` },
+                )
+                .setFooter({ text: `${interaction.user.displayName}`, iconURL: interaction.user.displayAvatarURL() })
+                .setTimestamp();
+    
+            await user.send({ embeds: [embed] })
+                .then(() => {
+                    interaction.reply({ content: `${emojis.custom.success} The message has been **successfully** sent to **${user.tag}**!`, ephemeral: true });
+                })
+                .catch(() => {
+                    interaction.reply({ content: `${emojis.custom.fail} **${user.tag}** has Direct Messages **disabled**!`, ephemeral: true });
+                });
+    
+        } else if (subcommand === 'toggle') {
+            const action = interaction.options.getString('action');
 
-		const embed = new EmbedBuilder()
-			.setTitle('`📮` Anonymous DM Received')
-			.setColor(`${color.default}`)
-			.addFields(
-				{ name: '`📝` Message', value: `${emojis.custom.replyend} ${message}` },
-				{ name: '`💻` Server', value: `${emojis.custom.replyend} ${interaction.guild.name}` }
-			)
-			.setFooter({ text: `Sent by Anonymous` })
-			.setTimestamp();
+            if (action === 'opt-out') {
+                // Opt-out: Set receiveDMs to false
+                const userId = interaction.user.id;
+                let userSettings = await UserSettingsSchema.findOne({ userId });
 
-		await user
-			.send({ embeds: [embed] })
-			.then(() => {
-				interaction.reply({
-					content: `${emojis.custom.success} The message has been **successfully** sent to **${user.tag}**!`,
-					ephemeral: true
-				});
-			})
-			.catch(() => {
-				interaction.reply({ content: `${emojis.custom.fail} **${user.tag}** has Direct Messages **disabled**!`, ephemeral: true });
-			});
-	}
+                if (!userSettings) {
+                    userSettings = new UserSettingsSchema({ userId, receiveDMs: false });
+                } else {
+                    userSettings.receiveDMs = false;
+                }
+
+                await userSettings.save();
+                interaction.reply({ content: `${emojis.custom.success} You have **opted out**. You can **no longer** receive Direct Messages from the command!`, ephemeral: true });
+            } else if (action === 'opt-in') {
+                // Opt-in: Set receiveDMs to true
+                const userId = interaction.user.id;
+                let userSettings = await UserSettingsSchema.findOne({ userId });
+
+                if (!userSettings) {
+                    userSettings = new UserSettingsSchema({ userId, receiveDMs: true });
+                } else {
+                    userSettings.receiveDMs = true;
+                }
+
+                await userSettings.save();
+                interaction.reply({ content: `${emojis.custom.success} You have **opted in**. You can now **receive** Direct Messages once again!`, ephemeral: true });
+            } else {
+                // Invalid action
+                interaction.reply({ content: `${emojis.custom.fail} Invalid action. Please choose either "opt-out" or "opt-in".`, ephemeral: true });
+            }
+        }
+    }
 }
 
 module.exports = {
-	UserCommand
+    UserCommand
 };
